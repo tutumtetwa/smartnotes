35237a97666ea24b630d625334e0b6b2dd1b3079
Tutu Mtetwa <tutumtetwa@gmail.com>
2025-05-22 20:30:26 -0500

    Fix: Bind Flask to 0.0.0.0 for Render

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 import spacy
+import os
 
 app = Flask(__name__)
 nlp = spacy.load("en_core_web_sm")
@@ -14,4 +15,6 @@ def index():
     return render_template('index.html', analysis=analysis)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
